16bddd88159f694d321c5c2dd12acdf1993f1417
Zackaria Antar <antarzack@gmail.com>
2023-04-23 10:23:48 -0500

    Created const maxItems and function isFull to check the number of items in the basket as length of array  against the  value of maxItems to return  true or false based on a conditional statement.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -19,6 +19,7 @@ addItem('Eggs')
 addItem('Bread')
 addItem('Soap')
 addItem('Sponges')
+addItem('Honey')
 
 console.log(basket)
 
@@ -40,39 +41,27 @@ listItems()
 
 function empty(){
      return basket=[];
-}
+};
 
 empty()
 console.log(basket)
 
-// > __IMPORTANT__
-// > Make sure that you are writing code *in the file* to test every function that you write!
-
-// For example, to test `addItem`:
-// ```
-// console.log(`Basket is ${basket}`);
-// console.log('Adding apples (expect true)', addItem('apples'));
-// console.log(`Basket is now ${basket}`);
-
-
-
-
-
-
-
-
-
-
-
-
-
 // __Using functions in other functions!__
-
 // 1. Add a global `const` named `maxItems` and set it to 5.
+
+const maxItems = 5;
 
 // 2. Create a function called isFull(). It should:
 //   - return `false` if the basket contains *less* than max number of items
 //   - return `true` otherwise (equal or more than maxItems)
+
+function isFull(){
+     if (basket.length>= maxItems){
+          return true;
+     }else return false;
+}
+
+console.log(isFull())
 
 // 3. Update the required `addItem` function to:
 //   - Use the `isFull` function to prevent more than `maxItems` from being added to the basket. 
